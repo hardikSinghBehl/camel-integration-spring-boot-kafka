c690e225d549afa34bf16bd52ee53cbcf1641cd0
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-05-21 13:59:44 +0530

    initializing superhero-searcher microservice

---
+++ superhero-searcher/src/main/java/com/behl/searcher/SuperheroSearcherApplication.java
@@ -0,0 +1,13 @@
+package com.behl.searcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroSearcherApplication.class, args);
+	}
+
+}
