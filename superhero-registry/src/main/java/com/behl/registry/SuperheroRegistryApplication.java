ac1b476bd9624b9e223ca130f45f39bcaabe6ce9
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-05-21 14:47:47 +0530

    initializing superhero-registry microservice

---
+++ superhero-registry/src/main/java/com/behl/registry/SuperheroRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.behl.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroRegistryApplication.class, args);
+	}
+
+}
