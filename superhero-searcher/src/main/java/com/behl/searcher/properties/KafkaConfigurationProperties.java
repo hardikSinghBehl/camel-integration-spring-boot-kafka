b5c26db6b02b3b5119eb897ce25c8e623dbc1403
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-05-21 15:27:33 +0530

    externalising kafka topic name to .properties

---
+++ superhero-searcher/src/main/java/com/behl/searcher/properties/KafkaConfigurationProperties.java
@@ -0,0 +1,13 @@
+package com.behl.searcher.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@Data
+@ConfigurationProperties(prefix = "com.behl.kafka")
+public class KafkaConfigurationProperties {
+
+	private String topicName;
+
+}
--- superhero-searcher/src/main/java/com/behl/searcher/route/SuperHeroSearchScheduler.java
+++ superhero-searcher/src/main/java/com/behl/searcher/route/SuperHeroSearchScheduler.java
@@ -1,10 +1,12 @@
 package com.behl.searcher.route;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import com.behl.searcher.dto.SuperHero;
 import com.behl.searcher.processor.MessageBodyLogger;
+import com.behl.searcher.properties.KafkaConfigurationProperties;
 import com.behl.searcher.transformer.SuperHeroSearcher;
 import com.behl.searcher.utility.JsonDataFormatter;
 
@@ -12,15 +14,18 @@ import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
+@EnableConfigurationProperties(value = KafkaConfigurationProperties.class)
 public class SuperHeroSearchScheduler extends RouteBuilder {
 
 	private final SuperHeroSearcher superHeroSearcher;
 	private final MessageBodyLogger messageBodyLogger;
+	private final KafkaConfigurationProperties kafkaConfigurationProperties;
 
 	@Override
 	public void configure() {
 		from("timer:superhero-search-scheduler?period=5000").bean(superHeroSearcher).process(messageBodyLogger)
-				.marshal(JsonDataFormatter.get(SuperHero.class)).to("kafka:superhero-information");
+				.marshal(JsonDataFormatter.get(SuperHero.class))
+				.to("kafka:" + kafkaConfigurationProperties.getTopicName());
 	}
 
 }
